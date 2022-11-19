869faac4d45552fe541a7538b617dbe7764d12e9
Fenna <fennnakadir@outlook.com>
2022-11-19 12:26:33 +0000

    check

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,9 @@
 var express = require("express");
-var User = require("../model/user");
 var router = express.Router();
 
-/* get all users */
-router.get("/", function (req, res, next) {});
-
-/* create new user*/
-router.post("/", function (req, res, next) {});
-
-/* update user*/
-// or put?
-router.patch("/", function (req, res, next) {});
-
-/* delete user*/
-router.post("/", function (req, res, next) {});
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
 
 module.exports = router;
