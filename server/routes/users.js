033220865e441eea5e74dd73ef305ea1684fe32e
Fenna <fennnakadir@outlook.com>
2022-11-20 19:59:40 +0000

    mongo finally connected

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,15 +3,28 @@ var User = require("../model/user");
 var mongoose = require("mongoose");
 var router = express.Router();
 
+// get and post methoddss
+// https://github.com/paul-kelly-dit/web-application-architecture/blob/master/mongo/mern-backend/routes/index.js
+
 /* get all users */
 router.get("/", function (req, res, next) {
-  console.log("arrived-get");
-  res.json({ mssg: "GET all users" });
+  let searchQuery = {};
+
+  if (req.query.username) searchQuery = { username: req.query.username };
+
+  User.find(searchQuery, function (err, users) {
+    if (err) {
+      res.status(400);
+      res.send();
+    }
+
+    console.log("returning all the users.");
+    res.send(users);
+  });
 });
 
 /* create new user*/
 router.post("/", function (req, res, next) {
-  console.log("arrived-post");
   let newUser = new User(req.body);
   newUser._id = mongoose.Types.ObjectId();
 
@@ -30,16 +43,33 @@ router.post("/", function (req, res, next) {
 /* update user*/
 // or put?
 router.patch("/", function (req, res, next) {
-  console.log("arrived-patch");
-  res.json({ mssg: "UPDATE user" });
+  let searchQuery = {};
+
+  if (req.query.id) searchQuery = { _id: req.query.id };
+
+  User.updateOne(searchQuery, { $set: req.body }, function (err, updated) {
+    if (err) {
+      res.status(400);
+      res.send();
+    }
+    res.send(updated);
+  });
 });
 
 /* delete user*/
 router.delete("/", function (req, res, next) {
-  console.log("arrived-post");
-  res.json({ mssg: "DELETE user" });
+  console.log("arrived-delete");
+  let searchQuery = {};
+
+  if (req.query.id) searchQuery = { _id: req.query.id };
+
+  User.deleteOne(searchQuery, function (err, updated) {
+    if (err) {
+      res.status(400);
+      res.send();
+    }
+    res.send(updated);
+  });
 });
 
 module.exports = router;
-
-// https://github.com/paul-kelly-dit/web-application-architecture/blob/master/mongo/mern-backend/routes/index.js
