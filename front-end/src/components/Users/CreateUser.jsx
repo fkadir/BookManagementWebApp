cb74cd90363be4c8218cb9e58060ab7ad36af3b8
Fenna <fennnakadir@outlook.com>
2022-11-22 14:13:42 +0000

    password validation, user creation implemented

--- front-end/src/components/Users/CreateUser.jsx
+++ front-end/src/components/Users/CreateUser.jsx
@@ -1,47 +1,35 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
 import { Button, Form } from "react-bootstrap";
 
-//potential for later when actually implementing it:
-// https://www.geeksforgeeks.org/how-to-show-and-hide-password-in-reactjs/ ???
+//password validation from: https://www.positronx.io/add-confirm-password-validation-in-react-with-hook-form/
 
 //ref: code from createuser lab4
 const CreateUser = () => {
   const [username, setUsername] = useState("");
-  const [pw, setPw] = useState("");
   const [email, setEmail] = useState("");
   const [statusMessage, setStatusMessage] = useState("");
 
-  const handleUsernameFieldChange = (event) => {
-    event.preventDefault();
-    setUsername(event.target.value);
-  };
+  const formSchema = Yup.object().shape({
+    password: Yup.string()
+      .required("Password is mandatory")
+      .min(6, "Password must be 6 char long"),
+    pwCheck: Yup.string()
+      .required("Password is mandatory")
+      .oneOf([Yup.ref("password")], "Password does not match"),
+  });
+  const formOptions = { resolver: yupResolver(formSchema) };
+  const { register, handleSubmit, reset, formState } = useForm(formOptions);
+  const { errors } = formState;
 
-  const handlePwFieldChange = (event) => {
-    event.preventDefault();
-    setPw(event.target.value);
-  };
-
-  // const handlePwCheck = (event) => {
-  //   event.preventDefault;
-  //   if (event.target.value == pw) {
-  //     return true;
-  //   } else return false;
-  // };
-
-  const handleEmailFieldChange = (event) => {
-    event.preventDefault();
-    setEmail(event.target.value);
-    console.log(email);
-  };
-
-  const handleCreateUser = async (event) => {
-    event.preventDefault();
-    console.log("clicked :)");
+  function onSubmit(data) {
     setStatusMessage("");
 
     let user = {
       username: username,
-      pw: pw,
+      pw: data.password,
       email: email,
     };
 
@@ -61,11 +49,21 @@ const CreateUser = () => {
       // Remediation logic
       setStatusMessage("There was an error creating the user");
     }
+  }
+
+  const handleEmailFieldChange = (event) => {
+    event.preventDefault();
+    setEmail(event.target.value);
+  };
+
+  const handleUsernameFieldChange = (event) => {
+    event.preventDefault();
+    setUsername(event.target.value);
   };
 
   return (
-    <div className="container mt-3">
-      <Form>
+    <div className="container mt-5">
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Form.Group>
           <Form.Label>Email address</Form.Label>
           <Form.Control
@@ -89,22 +87,31 @@ const CreateUser = () => {
         <Form.Group>
           <Form.Label>Password</Form.Label>
           <Form.Control
-            type="text"
-            placeholder=""
-            value={pw}
-            onChange={(e) => handlePwFieldChange(e)}
+            name="password"
+            type="password"
+            {...register("password")}
+            className={`form-control ${errors.password ? "is-invalid" : ""}`}
           />
+          <div className="invalid-feedback">{errors.password?.message}</div>
         </Form.Group>
 
-        {/* validation! */}
         <Form.Group>
-          <Form.Label>Repeat Password</Form.Label>
-          <Form.Control type="text" placeholder="" />
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            name="pwCheck"
+            type="password"
+            {...register("pwCheck")}
+            className={`form-control ${errors.pwCheck ? "is-invalid" : ""}`}
+          />
+          <div className="invalid-feedback">{errors.pwCheck?.message}</div>
         </Form.Group>
-        <Button variant="light" onClick={handleCreateUser}>
-          {" "}
-          create account
-        </Button>
+
+        <div className="mt-3">
+          <Button type="submit" variant="light">
+            {" "}
+            create account
+          </Button>
+        </div>
       </Form>
       {/* forgot password??? */}
     </div>
@@ -112,45 +119,3 @@ const CreateUser = () => {
 };
 
 export default CreateUser;
-
-{
-  /* <div className="">
-<div className="">
-  <label className="">Email</label>
-  <input
-    type="text"
-    placeholder="name@example.com"
-    value={email}
-    onChange={(e) => handleEmailFieldChange(e)}
-  />
-
-  <label className="">Username</label>
-  <input
-    type="text"
-    placeholder=""
-    value={username}
-    onChange={(e) => handleUsernameFieldChange(e)}
-  />
-
-  <label className="">Password</label>
-  <input
-    type="text"
-    placeholder=""
-    value={pw}
-    onChange={(e) => handlePwFieldChange(e)}
-  />
-
-  <label className="">Repeat Password</label>
-  <input
-    type="text"
-    placeholder=""
-    value={pw}
-    onChange={(e) => handlePwFieldChange(e)}
-  />
-  <button className="" onClick={handleCreateUser}>
-    Create User
-  </button>
-  <p className="">{statusMessage}</p>
-</div>
-</div> */
-}
